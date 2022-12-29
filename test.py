e64af9551e3248fed91f022f98eba73f69f362e2
DAF201 <daf201@blink-in.com>
2022-12-29 13:20:59 -0600

    add token and github test

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
-import requests
-
-re = requests.post("http://127.0.0.1/APIs/blink_in",
-                   files=[('file', open('test.py', 'rb')), ('file', open('test.png', 'rb'))]).text
-print(re)
+from github import Github
+with open('./static_files/github_token', 'r')as token:
+    token = token.read()
+g = Github(login_or_token=token)
+user = g.get_user()
+print(user)
+login = user.login
+print(user)
+print(login)
