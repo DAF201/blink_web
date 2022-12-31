33137bc170f54ce47cd81dd5e66f1144f604f23f
DAF201 <daf201@blink-in.com>
2022-12-31 15:21:37 -0600

    Update test.py

--- test.py
+++ test.py
@@ -1,23 +1,12 @@
-from src.tools import *
-from github import Github
-import base64
 import requests
 
-# with open("test.py", 'rb')as test_file:
-#     test_file = test_file.read()
-# re = requests.post("http://127.0.0.1/APIs/blink_in",
-#                    files={'file': test_file})
-# print(re.text)
+with open("music_playlist\I want to be happy.mp3", 'rb')as test_file:
+    test_file = test_file.read()
+re = requests.post("http://127.0.0.1/APIs/blink_in",
+                   files={'file': test_file})
+print(re.text)
 
 re = requests.get("http://127.0.0.1/APIs/blink_in",
-                  params={'file': '49e7d58783980862ae84bcd1580aa02a.png'})
-print(re.content)
-
-# git = Github(login_or_token='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')
-# user = git.get_user()
-# repo = user.get_repo('blink_in_inventory')
-# data = base64.b64decode(repo.get_contents(
-#     '49e7d58783980862ae84bcd1580aa02a.png').raw_data['content'])
-# print(data)
-# with open('test.txt', 'wb')as test:
-#     test.write(blink_in_decode(data))
+                  params={'file': '2a2673e31f225f463dff5b989aab9b7e'})
+with open('test.mp3', 'wb')as test:
+    test.write(re.content)
