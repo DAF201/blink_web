a1fc3a87e0568ec75eedaa19693419b258858bee
daf201 <daf201@blink-in.com>
2023-01-10 15:50:32 -0600

    Update test.py

--- test.py
+++ test.py
@@ -2,11 +2,11 @@ import requests
 
 with open("music_playlist\I want to be happy.mp3", 'rb')as test_file:
     test_file = test_file.read()
-re = requests.post("http://127.0.0.1/APIs/blink_in",
+re = requests.post("https://127.0.0.1/APIs/blink_in",
                    files={'file': test_file})
 print(re.text)
 
-re = requests.get("http://127.0.0.1/APIs/blink_in",
+re = requests.get("https://127.0.0.1/APIs/blink_in",
                   params={'file': '2a2673e31f225f463dff5b989aab9b7e'})
 with open('test.mp3', 'wb')as test:
     test.write(re.content)
